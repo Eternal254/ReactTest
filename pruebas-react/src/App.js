414b3cef5fe2e04437b02c68be6d7fb19990b8e2
Eternal254 <137845281+Eternal254@users.noreply.github.com>
2024-02-14 18:40:16 -0600

    Comienzo
    Aqui se realizo la instalacion basica y el inicio del proyecto

---
+++ pruebas-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Aqui un simple mensaje para que comience todo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
