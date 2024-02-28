a5fbde542e5e705bd2a491da51796c93f1983234
Eternal254 <137845281+Eternal254@users.noreply.github.com>
2024-02-27 21:29:03 -0600

    Mini update
    Algunos ligeros cambios de cosas menores

--- pruebas-react/src/pages/Dashboard.js
+++ pruebas-react/src/pages/Dashboard.js
@@ -3,8 +3,11 @@ import '../App.css'
 
 var beasts = [
     {name: "Dragon", description: "Bestia de gran tamaño con la capacidad de volar y escupir fuego", image: "https://www.nameoftheyear.com/wp-content/uploads/2024/01/image-50.jpeg" },
-    {name: "Orco", description: "Criatura musculosa de piel verde, con dientes prominentes, orejas puntiagudas y agresiva", image:"https://posadaelultimohogar.files.wordpress.com/2015/01/orco.jpg?w=640" },
-    {name: "Pegaso", description: "Caballo con alas mamalon xd", image:"https://static.tvtropes.org/pmwiki/pub/images/concordia_pegasus_ravnica_allegiance_art.jpg"}
+    {name: "Orco", description: "Criatura musculosa de piel verde, con dientes prominentes, orejas puntiagudas y agresiva", image:"https://img.freepik.com/fotos-premium/guerrero-orco-pelirrojo_880127-330.jpg" },
+    {name: "Pegaso", description: "Caballo con alas mamalon xd", image:"https://static.tvtropes.org/pmwiki/pub/images/concordia_pegasus_ravnica_allegiance_art.jpg"},
+    {name: "Undead", description: "Cadaver reanimado por energias oscuras", image: "https://diamondpaintkit.com/wp-content/uploads/2022/02/Fantasy-Undead-diamond-painting.jpg"},
+    {name: "Medusa", description: "Criatura oscura con mirada petrea", image: "https://www.mitologiafantastica.com/wp-content/uploads/2023/09/medusa-mitoligia.jpg.webp"},
+    {name: "Hidra de lerma", description: 'Bestia legendaria de gran tamaño. "Y por cada cabeza cortada, a esa cosa le crecian 3 mas".', image: "https://2.bp.blogspot.com/-Y8acEL6q3NU/U76821axHFI/AAAAAAAAQMQ/KQIXLuqSpsM/s1600/011113_HIDRA.jpg"}
 ]
 
 const Dashboard = () => {
